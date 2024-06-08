4837d9feaf3fffac4b44270f51e7a78fcea88330
Thiago Melo <4thiagomelo5@gmail.com>
2024-06-08 16:33:00 -0300

    install and config axis

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,3 +6,5 @@ import './../../vendor/power-components/livewire-powergrid/dist/tailwind.css'
 import flatpickr from "flatpickr";
 import 'flatpickr/dist/flatpickr.min.css';
 
+import Chart from 'chart.js/auto';
+window.Chart = Chart;

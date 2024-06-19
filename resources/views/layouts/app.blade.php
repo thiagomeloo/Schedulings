a2472f25e117a574c7f60cb3dd18a9e8258cfa83
Thiago Melo <4thiagomelo5@gmail.com>
2024-06-19 20:48:08 -0300

    update layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,7 +40,23 @@
 
         <!-- Page Content -->
         <main>
-            {{ $slot }}
+            @if (isset($header))
+                <x-slot name="header">
+                    <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+                        {{ $header }}
+                    </h2>
+                </x-slot>
+            @endif
+
+            <div class="py-12">
+                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div class="p-6 text-gray-900 dark:text-gray-100">
+                            {{ $slot }}
+                        </div>
+                    </div>
+                </div>
+            </div>
         </main>
     </div>
     @livewireScripts

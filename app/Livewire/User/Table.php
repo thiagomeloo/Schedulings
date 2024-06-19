65a9ab2ac517d1dc6aac9ce6aae9ced514bbf563
Thiago Melo <4thiagomelo5@gmail.com>
2024-06-19 20:50:05 -0300

    start user crud

---
+++ app/Livewire/User/Table.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Livewire\User;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Carbon;
+use PowerComponents\LivewirePowerGrid\{
+    Column,
+    Footer,
+    PowerGrid,
+    PowerGridFields,
+    PowerGridComponent
+};
+
+final class Table extends PowerGridComponent
+{
+    public function setUp(): array
+    {
+        $this->showCheckBox();
+
+        return [
+            Footer::make()
+                ->showPerPage()
+                ->showRecordCount(),
+        ];
+    }
+
+    public function datasource(): Builder
+    {
+        return User::query();
+    }
+
+    public function fields(): PowerGridFields
+    {
+        return PowerGrid::fields()
+            ->add('id')
+            ->add('name')
+            ->add('name_lower', fn (User $model) => strtolower(e($model->name)))
+            ->add('created_at',  fn (User $model) => Carbon::parse($model->created_at)->format('d/m/Y H:i:s'));
+    }
+
+    public function columns(): array
+    {
+        return [
+            Column::make('ID', 'id')
+                ->searchable()
+                ->sortable(),
+
+            Column::make('Name', 'name')
+                ->searchable()
+                ->sortable(),
+
+            Column::make('Created at', 'created_at', 'created_at')
+                ->searchable(),
+        ];
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\User\Index;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -22,6 +23,13 @@ Route::middleware('auth')->group(function () {
                 Route::delete('/', 'destroy')->name('destroy');
             });
         });
+
+    Route::prefix('user')
+        ->name('user.')
+        ->group(function(){
+            Route::get('/', Index::class)->name('index');
+        });
+
 });
 
 require __DIR__.'/auth.php';
